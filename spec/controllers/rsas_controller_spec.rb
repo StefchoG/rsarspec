cefbe23da1f411700cf69676084fde32e7cf75ac
StefchoG <ste.grozev.tues19@gmail.com>
2018-01-14 23:09:54 +0200

    Homework

---
+++ spec/controllers/rsas_controller_spec.rb
@@ -0,0 +1,9 @@
+"type::controller"
+RSpec.describe RsasController do
+
+it "shows an rsa" do
+get :show, params: {id: 1}
+expect(response).to render_template :show
+end
+
+end
---
+++ spec/models/rsa_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe Rsa do
+
+it "encrypts" do
+r = Rsa.new
+expect(r.encrypt("blabla")).to eq "qwer"
+end
+
+
+
+end
